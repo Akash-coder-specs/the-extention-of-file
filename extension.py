e7d73ec6e001d74425f4c7423418b63d08f801c7
Akash-coder-specs <67906323+Akash-coder-specs@users.noreply.github.com>
2020-10-08 12:23:45 +0100

    this shows the extension of the file

---
+++ extension.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  8 12:01:02 2020
+
+@author: AKASH
+"""
+import os
+a=str(input("enter the file name:"))
+file,file_extension=os.path.splitext(a)
+print(f"The extension of file is {file_extension} which is 'python'")
+
+
+
+
